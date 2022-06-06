7e1a4bf0fe57fd0f766a289a0d3923792a902dad
润心 <maosicheng98@gmail.com>
2022-06-07 00:20:51 +0200

    添加了三分损益法

--- Temperament.py
+++ Temperament.py
@@ -36,7 +36,31 @@ class Temperament:
       return cls(just_ratios)
 
    @classmethod
-   def PythagorasTemperament(cls):
+   def SanfenSunyifa(cls): #每步或损或益，但均保持管长比介于二分之一与一之间。
+      Huangzhong = 81
+      QingHuangzhong = Huangzhong/2
+      yin = Huangzhong
+      GuanChang = [Huangzhong]
+      num = 0
+      while(num < Temperament.num):
+         sun = yin * 2/3
+         yi  = yin * 4/3
+         if sun < QingHuangzhong:
+            yin = yi
+         elif yi > Huangzhong:
+            yin = sun
+         else: #only happens when yin = 3/4
+            pass #no else in execution for yin never equal to 3/4
+         num += 1
+         GuanChang.append(yin)
+      GuanChang[-1] /= 2 #最后一个须是清黄钟，手动调整
+      ratios = [81/GuanChang[i] for i in range(len(GuanChang))]
+      ratios.sort()
+      print(GuanChang)
+      return cls(ratios)
+
+   @classmethod
+   def PythagorasTemperament(cls): #each time raise a perfect fifth, always keeping the frequency ratio inside [1,2]
       ratios = [0] * 13
       ratios[0] = 1
       log2_perfect_fifth = math.log2(3/2)
@@ -47,6 +71,8 @@ class Temperament:
       ratios[12] =  2 * (3 ** 12 / 2 ** (12 + math.floor(12 * log2_perfect_fifth)))
       return cls(ratios)
    
+   #SanfenSunyifa is essentially similar to the pythagoras temperament but with different points of view
+
    @classmethod
    def EqualTemperament(cls):
       ratio = 2 ** (1/Temperament.num) # 朱载堉
